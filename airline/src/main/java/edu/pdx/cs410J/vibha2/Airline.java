62628590bf661b9695a5ed7c612e44dcc1a54775
VinayaBhat <vibha2@pdx.edu>
2020-01-18 22:06:28 -0800

    Flight modified

---
+++ airline/src/main/java/edu/pdx/cs410J/vibha2/Airline.java
@@ -0,0 +1,34 @@
+package edu.pdx.cs410J.vibha2;
+
+import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AbstractFlight;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Airline extends AbstractAirline {
+    //Properties of Airline
+    String airline_name;
+    List<AbstractFlight> flights=new ArrayList<>();
+
+    //Airline constructor
+    public Airline(String name){
+        this.airline_name=name;
+    }
+
+    @Override
+    public String getName() {
+        return this.airline_name;
+    }
+
+    @Override
+    public void addFlight(AbstractFlight abstractFlight) {
+        flights.add(abstractFlight);
+    }
+
+    @Override
+    public Collection getFlights() {
+        return flights;
+    }
+}
--- airline/src/main/java/edu/pdx/cs410J/vibha2/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/vibha2/Flight.java
@@ -3,28 +3,69 @@ package edu.pdx.cs410J.vibha2;
 import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+  //properties
+  int flight_number;
+  String source;
+  String destination;
+  String arrival_time;
+  String departure_time;
+
+  //constructor
+  public Flight(int flightNumber){
+    this.flight_number=flightNumber;
+  }
+
+  //Setters
+  public void setSource(String source){
+    this.source=source;
+  }
+
+  public void setDestination(String destination){
+    this.destination=destination;
+  }
+
+  public void setArrival_time(String arrivalTime){
+    this.arrival_time=arrivalTime;
+  }
+
+  public void setDeparture_time(String departureTime){
+    this.departure_time=departureTime;
+  }
+
   @Override
   public int getNumber() {
-    return 42;
+    return this.flight_number;
   }
 
   @Override
   public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if(source!=null)
+    return this.source;
+    else
+      throw new UnsupportedOperationException("Source not specified");
   }
 
   @Override
   public String getDepartureString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if(departure_time!=null)
+      return this.departure_time;
+    else
+      throw new UnsupportedOperationException("Departure Time not specified");
   }
 
   @Override
   public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if(destination!=null)
+      return this.destination;
+    else
+      throw new UnsupportedOperationException("Destination not specified");
   }
 
   @Override
   public String getArrivalString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    if(arrival_time!=null)
+      return this.arrival_time;
+    else
+      throw new UnsupportedOperationException("Arrival Time not specified");
   }
 }
